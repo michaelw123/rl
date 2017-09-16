af09e92c5464f3e567b7d5dae35efc10c7cfec7f
michaelw123 <xiaoqunw@hotmail.com>
2017-09-16 18:14:31 -0400

    init chechin

--- connect5/src/main/scala/rl/connect5/connect5.scala
+++ connect5/src/main/scala/rl/connect5/connect5.scala
@@ -8,20 +8,26 @@ import breeze.linalg._
 
 
 object connect5 extends App{
-  println("hello")
 
-  val data = DenseMatrix.zeros(10, 10)
+  val data = DenseMatrix.zeros[Int](5, 5)
+ // println(data)
+
+  val state=State
+  print(state.winner)
+  //(0,0,true,DenseMatrix.zeros[Int](10, 10))
+
+
 }
-case class State(winner:Int=0, var hashVal:Int =0, end:Boolean = false, data:DenseMatrix[Int]=DenseMatrix.zeros(10, 10)) {
+case class State(winner:Int=0,  hashVal:Int =0,  end:Boolean = false,  data:DenseMatrix[Int]=DenseMatrix.zeros[Int](10, 10)) {
 
-  def getHash: Int = {
-    if (hashVal == 0 ) {
-      for( i <- data.reshape(10,10)) {
-        hashVal = hashVal *3 + i
-      }
-    }
-    hashVal
-  }
+//  def getHash: Int = {
+//    if (hashVal == 0 ) {
+//      for( i <- data.reshape(data.rows,data.cols)) {
+//        hashVal = hashVal *3 + i
+//      }
+//    }
+//    hashVal
+//  }
 //  def isEnd:Boolean = {
 //    checkColumn && checkRow && checkDiagnol
 //  }
@@ -30,4 +36,7 @@ case class State(winner:Int=0, var hashVal:Int =0, end:Boolean = false, data:Den
 //      rows.
 //    }
 //  }
+
+
+
 }
