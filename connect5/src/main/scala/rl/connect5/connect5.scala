81232c243f577d1a2b5ad7bc605cebe197ed89f5
michaelw123 <xiaoqunw@hotmail.com>
2017-09-16 16:37:10 -0400

    init chechin

--- connect5/src/main/scala/rl/connect5/connect5.scala
+++ connect5/src/main/scala/rl/connect5/connect5.scala
@@ -5,6 +5,29 @@ import breeze.linalg._
 /**
   * Created by MichaelXiaoqun on 2017-09-16.
   */
+
+
 object connect5 extends App{
-println("hello")
+  println("hello")
+
+  val data = DenseMatrix.zeros(10, 10)
+}
+case class State(winner:Int=0, var hashVal:Int =0, end:Boolean = false, data:DenseMatrix[Int]=DenseMatrix.zeros(10, 10)) {
+
+  def getHash: Int = {
+    if (hashVal == 0 ) {
+      for( i <- data.reshape(10,10)) {
+        hashVal = hashVal *3 + i
+      }
+    }
+    hashVal
+  }
+//  def isEnd:Boolean = {
+//    checkColumn && checkRow && checkDiagnol
+//  }
+//  def checkColumn:Boolean = {
+//    for (rows <- data.rows  ) {
+//      rows.
+//    }
+//  }
 }
