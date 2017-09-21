8a8f3a7332d713e64ccf23bd16fb84ddc8802c74
Michael Wang <xiaoqunw@hotmail.com>
2017-09-21 16:41:15 -0400

    init chechin

--- connect5/src/main/scala/rl/tictactoe/TicTacToe.scala
+++ connect5/src/main/scala/rl/tictactoe/TicTacToe.scala
@@ -18,7 +18,7 @@ import breeze.linalg.DenseMatrix
 
 case class State( data:DenseMatrix[Int]) {
     val hashVal:Int = hashCode
-    override def hashCode: Int = {
+  override def hashCode: Int = {
       if (hashVal == 0 ) {
         data.toArray.foldLeft(0)(_*3 + _+1)
       } else {
@@ -28,29 +28,28 @@ case class State( data:DenseMatrix[Int]) {
   def isEnd:Boolean = winner!=0
 
   def winner:Int = {
-     val rows = data(*, ::)
+    var w:Int = winner1(data)
+    if (w==0) w = winner1(data.t)
+    if (w==0) {
+      val x = data(0, 0) + data(1, 1) + data(2, 2)
+      val y = data(2, 0) + data(1, 1) + data(0, 2)
+      println(x, y)
+      if (x == 3 || y == 3)
+        w = 1
+      else if (x == -3 || y == -3)
+        w = -1
+    }
+    w
+  }
+  def winner1(d:DenseMatrix[Int]): Int ={
+    val rows = data(*, ::)
     for (row <- rows) {
       println(sum(row))
-     if (sum(row) == 3)
-       return 1
-     else if (sum(row) == -3)
-       return -1
-    }
-    val cols = data(::, *)
-    for (col <- cols) {
-      println(sum(col))
-      if (sum(col) == 3)
+      if (sum(row) == 3)
         return 1
-      else if (sum(col) == -3)
+      else if (sum(row) == -3)
         return -1
     }
-    val x = data(0,0) + data(1,1)+ data(2, 2)
-    val y = data(2,0) + data(1,1)+ data(0, 2)
-    println(x, y)
-    if (x == 3 || y == 3)
-      return 1
-    else if (x == -3 || y == -3)
-      return -1
-     0
+    0
   }
 }
