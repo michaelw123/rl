b3c41cce6875d10dda32512c38b626a27ee623cf
Michael Wang <xiaoqunw@hotmail.com>
2017-10-25 13:21:18 -0400

    init chechin

--- connect5/src/main/scala/rl/connect5/connect5.scala
+++ connect5/src/main/scala/rl/connect5/connect5.scala
@@ -1,86 +1,57 @@
 package rl.connect5
 import java.io.{FileInputStream, FileOutputStream, ObjectInputStream, ObjectOutputStream}
 
-import breeze.linalg.{DenseMatrix, DenseVector, _}
-import rl.tictactoe.game.ROWCOL
+import breeze.linalg._
+import rl.connect5.game.ROWCOL
 
+import scala.annotation.tailrec
 import scala.collection.{immutable, mutable}
 import scala.util.Random
 
 /**
-  * Created by wangmich on 09/19/2017.
+  * Created by Michael Wang on 09/19/2017.
   */
 object connect5 extends App {
-  game train
-  //game play
+  //game train
+  game play
 }
 import breeze.linalg.DenseMatrix
 import breeze.stats.distributions.Binomial
 
 case class State( data:DenseMatrix[Int]) {
   val hashVal:Int = hashCode
-  override def hashCode: Int = {
-    if (hashVal == 0 ) {
-      data.toArray.foldLeft(0)(_*3 + _+1)
-    } else {
-      hashVal
-    }
+  override def hashCode: Int = hashVal match {
+    case 0 => data.toArray.foldLeft(0)(_ * 3 + _ + 1)
+    case _ => hashVal
   }
   def winner:Int = {
-    var w:Int = winner(data)
-    if (w==0) w = winner(data.t)
-    if (w==0) w=diagWinner(data)
+    var w: Int = winner(data)
+    if (w == 0) w = winner(rot90(data))
+    if (w == 0) {
+      val x = trace(data)
+      val y = trace(rot90(data))
+      w = (math.abs(x), math.abs(y)) match {
+        case (data.rows, _) => math.signum(x)
+        case (_, data.rows) => math.signum(y)
+        case _ => 0
+      }
+    }
     w
   }
-  def diagWinner(m:DenseMatrix[Int]): Int = {
-    if (m.rows == 5) {
-      return diagWinner5(m)
-    }
-    val m1 = m(0 to m.rows-1,0 to m.rows-1)
-    val m2 = m(0 to m.rows, 1 to m.rows)
-    val m3 = m(1 to m.rows, 0 to m.rows-1)
-    val m4 = m(1 to m.rows, 1 to m.rows)
-    val x = Seq(m1, m2, m3, m4).foldLeft(0){
-      (a, b) => {
-        if (a == 0) {
-          val x = diagWinner(b)
-          if (math.abs(x) == 5) {
-            math.signum(x)
-          } else {
-            0
-          }
-        } else {
-          a
-        }
-      }
-    }
-    x
-  }
-  def diagWinner5(m:DenseMatrix[Int]): Int = {
-    val x = trace(m)
-    if (math.abs(x) == 5 )
-      return math.signum(x)
-    val y = trace(m.t)
-    if (math.abs(y) == 5 )
-      return math.signum(y)
-    0
-  }
-  def winner(m:DenseMatrix[Int]): Int ={
-    val rows = m(*, ::)
+  def winner(d:DenseMatrix[Int]): Int ={
+    val rows = d(*, ::)
     for (row <- rows) {
-      val x = winner(row)
-      if (winner(row) != 0) return math.signum(x)
+      val w = check5(row)
+      if (w!=0) return w
     }
     0
   }
-
-  def winner(v:DenseVector[Int]): Int = {
-    for (start <- 0 to v.length-5) {
-      val thesum = sum(v(start to start+4))
-      if (math.abs(thesum) == 5) {
-        return math.signum(thesum)
-      }
-    }
+  private def check5(v:DenseVector[Int]):Int = {
+    v.foldLeft(0)((a, b) => {
+      if (math.signum(a) != math.signum(b)) b
+      if (math.abs(a) ==game.FIVE -1 ) return math.signum(a)
+      a+b
+    })
     0
   }
   def nextState(i:Int, j:Int, player:Int): State = {
@@ -88,126 +59,109 @@ case class State( data:DenseMatrix[Int]) {
     newData(i,j) = player
     State(newData)
   }
-  def show:Unit = {
-    println(data)
-  }
+  def show = println(data)
+  def isTie:Boolean = isEnd && (winner == 0)
+  def isEnd:Boolean = data.toArray.count(_ == 0) == 0
 }
-
 sealed class Player (val playerSymbol:Int, val exploreRate:Int){
   val stepsize=0.1
+  val estimations = mutable.HashMap.empty[Int, Double]
+  val states = mutable.LinkedHashMap.empty[Int, State]
 
-  var estimations:mutable.HashMap[Int, Double] = mutable.HashMap[Int, Double]()
-  var states:mutable.LinkedHashMap[Int, State] = mutable.LinkedHashMap[Int, State]()
+  def currentData = states.isEmpty match {
+    case true => DenseMatrix.zeros[Int](game.ROWCOL, game.ROWCOL)
+    case _ => currentState.data
+  }
 
+  def currentState = states.isEmpty match {
+    case true => State(DenseMatrix.zeros[Int](game.ROWCOL, game.ROWCOL))
+    case false => states.values.last
+  }
   def feedState(state:State): Unit = {
     states.put(state.hashCode, state)
-    //estimations.put(state.hashCode, 0)
-  }
-  def feedReward(reward:Double):Unit = {
-    if (states.isEmpty) return
-    val key = states.keySet.last
-    if (estimations.contains(key)) {
-      estimations(key) = estimations(key) + stepsize * (reward - estimations(key))
-    } else {
-      estimations.put(key, reward)
-    }
-    feedReward(estimations(key), states.dropRight(1))
   }
   def feedReward(reward:Double, theStates:mutable.LinkedHashMap[Int, State]):Unit = {
-    if (theStates.isEmpty) return
-    val key=theStates.keySet.last
-    if (estimations.contains(key)) {
+    if (!theStates.isEmpty) {
+      val (key, theState) = theStates.last
+      if (!estimations.contains(key)) {
+        estimations.put(key, (theState.isEnd, theState.winner) match {
+          case (true, playerSymbol) => 1
+          case (true, _) => 0
+          case (fasle, _) => 0.5
+        })
+      }
       estimations(key) = estimations(key) + stepsize * (reward - estimations(key))
-    } else {
-      estimations.put(key, reward)
+      feedReward(estimations(key), theStates.dropRight(1))
     }
-    feedReward(estimations(key), theStates.dropRight(1))
   }
-
   def takeAction = {
     val (i, j) = nextPosition
-    val nextState = nextState1(i,j)
-
-    states.put(nextState.hashCode, nextState)
-    (i, j, nextState)
+    val nState = nextState(i,j)
+    states.put(nState.hashCode, nState)
+    (i, j, nState)
   }
-  def nextState1(i:Int,j:Int):State = {
-    if (states.isEmpty){
+  def nextState(i:Int,j:Int):State = states.isEmpty match {
+    case true => {
       val newData = DenseMatrix.zeros[Int](ROWCOL, ROWCOL)
       newData(i,j) = playerSymbol
       State(newData)
-    } else {
-      states.values.last.nextState(i, j, playerSymbol)
     }
+    case _ => currentState.nextState(i, j, playerSymbol)
   }
-  def toExplore:Boolean = {
-    if (exploreRate==0) {
-       false
-    } else {
-       Binomial(100, exploreRate).draw <= 100 * exploreRate
-    }
+  def toExplore:Boolean = exploreRate match {
+    case 0 => false
+    case _: Int => Binomial(100, exploreRate).draw <= 100 * exploreRate
   }
-  private def nextPosition:(Int, Int) = {
-    if (states.nonEmpty ) {
-      if (toExplore) {
-        explore
-      } else {
-        exploite
-      }
-    } else {
-      (Random.nextInt(game.ROWCOL), Random.nextInt(game.ROWCOL))
-    }
+  private def nextPosition:(Int, Int) = toExplore match {
+    case true => explore
+    case _ => exploite
   }
   private def explore:(Int, Int) = {
-    val latestStateData = states.values.last.data
-    val a = latestStateData.toArray
-    val size = a.count(_ == 0)
-    val index = Random.nextInt(size)
+    val a = currentData.toArray.count(_ == 0)
+    val index = Random.nextInt(a)
     var x = 0
-    for (i <- 0 to ROWCOL * ROWCOL) {
-      if (latestStateData.valueAt(i) == 0) {
-        if (x == index) return latestStateData.rowColumnFromLinearIndex(i)
-        x = x + 1
+    for (i <- 0 to ROWCOL * ROWCOL -1 if currentData.valueAt(i) == 0) {
+      if (x == index) {
+        return currentData.rowColumnFromLinearIndex(i)
       }
+      x = x + 1
     }
-    (0, 0)
+    currentData.rowColumnFromLinearIndex(x)
   }
   private def exploite:(Int, Int) = {
     val availablePositions = mutable.LinkedHashMap[Int, Double]()
-    val data = states.values.last.data
+    val data = currentData
     val latestStateData = data.toArray
-
-    for (i <- 0 until ROWCOL * ROWCOL -1) {
-      if (latestStateData(i) == 0) {
-        val newData = states.values.last.data.copy
-        newData(data.rowColumnFromLinearIndex(i))=playerSymbol
-        val newState=State(newData)
-        val hash = newState.hashCode()
-        if (estimations.contains(hash)) {
-          availablePositions.put(i, estimations(hash))
-        } else {
-          availablePositions.put(i, 0)
-        }
-      }
+    for (i <- 0 to ROWCOL * ROWCOL -1 if latestStateData(i) == 0) {
+      val newData = data.copy
+      newData(data.rowColumnFromLinearIndex(i))=playerSymbol
+      val newState=State(newData)
+      val hash = newState.hashVal
+      availablePositions.put(i, estimations.contains(hash) match {
+        case true => estimations(hash)
+        case _ => 0
+      })
     }
     val max = availablePositions.maxBy(_._2)
-    println(max)
     data.rowColumnFromLinearIndex(max._1)
   }
   def isTie:Boolean = {
-    !states.values.last.data.toArray.contains(0)
+    currentState.isTie
   }
-  def show:Unit = println(states.values.last.data)
-  def savePolicy(file:String):Unit = {
+  def isEnd:Boolean = {
+    currentState.isEnd
+  }
+  def show = println(currentData)
+  def savePolicy(file:String) = {
+    println("estimations size:"+estimations.size)
     val oos = new ObjectOutputStream(new FileOutputStream(file))
     oos.writeObject(estimations)
     oos.close
   }
-  def loadPolicy(file:String):Unit= {
+  def loadPolicy(file:String)= {
     val ois = new ObjectInputStream(new FileInputStream(file))
-    estimations = ois.readObject.asInstanceOf[mutable.HashMap[Int, Double]]
+    estimations ++= ois.readObject.asInstanceOf[mutable.HashMap[Int, Double]]
     ois.close
-
   }
 }
 object Player {
@@ -220,59 +174,52 @@ object Player {
       println("enter the position:")
       val input:Int = scala.io.StdIn.readLine().toInt
       println(input)
-      val (i,j) = states.values.last.data.rowColumnFromLinearIndex(input)
-      val nextState = nextState1(i,j)
-
-      states.put(nextState.hashCode, nextState)
-      (i, j, nextState)
+      val (i,j) = currentData.rowColumnFromLinearIndex(input)
+      val nState = nextState(i,j)
+      states.put(nState.hashCode, nState)
+      (i, j, nState)
     }
-
     override def feedReward(reward:Double, theStates:mutable.LinkedHashMap[Int, State]): Unit = {
-    }
-    override def feedReward(reward:Double): Unit = {
     }
   }
 }
 object game {
   final val ROWCOL = 10
+  final val FIVE=5
   def findRewards(p1:Player, p2:Player, theWinner:Int) = {
-    if (p1.playerSymbol == theWinner) {
-      (1.0, 0.0)
-    } else if (p2.playerSymbol == theWinner) {
-      (0.0, 1.0)
-    } else {
-      (0.5, 0.5)
+    theWinner match {
+      case p1.playerSymbol => (1.0, 0.0)
+      case p2.playerSymbol => (0.0, 1.0)
+      case _ => (0.5, 0.5)
     }
   }
-
   def loadPolicy(file:String): mutable.HashMap[Int, Double]= {
     val ois = new ObjectInputStream(new FileInputStream(file))
     val policy = ois.readObject.asInstanceOf[mutable.HashMap[Int, Double]]
     ois.close
     policy
   }
+  @tailrec
   def go(p1:Player, p2:Player):Unit = {
     val (i, j, state) = p1.takeAction
     p1.feedState(state)
     p2.feedState(state)
     val winner = state.winner
-    if (winner!=0)  {
-      val (reward, otherReward) = findRewards(p1, p2, winner)
-      p1.feedReward(reward)
-      p2.feedReward(otherReward)
-      println("found winner:"+winner)
-      p1.show
-    } else {
-      if (!p1.isTie) go(p2, p1)
+    winner match {
+      case 0 => if (!p1.isEnd) go(p2, p1)
+      case _ => val (reward, otherReward) = findRewards(p1, p2, winner)
+        p1.feedReward(reward, p1.states)
+        p2.feedReward(otherReward, p2.states)
+        println("found winner:"+winner)
+        p1.show
     }
   }
-  def train(implicit data: DenseMatrix[Int] = DenseMatrix.zeros[Int](ROWCOL, ROWCOL)):Unit = {
-    val player:Player = Player.ai1
-    val otherPlayer:Player = Player.ai2
-
-    for (i <- 0 to 50000) {
-      player.states= player.states.empty
-      otherPlayer.states= otherPlayer.states.empty
+  def train(implicit data: DenseMatrix[Int] = DenseMatrix.zeros[Int](ROWCOL, ROWCOL)) = {
+    val player = Player.ai1
+    val otherPlayer = Player.ai2
+    for (i <- 0 to 400000) {
+      player.states.clear
+      otherPlayer.states.clear
       go(player, otherPlayer)
       System.out.println("Epoch "+i)
     }
@@ -281,11 +228,12 @@ object game {
     println(otherPlayer.estimations)
     otherPlayer savePolicy "c://work/tmp/player2-policy"
   }
-  def play(implicit data: DenseMatrix[Int] = DenseMatrix.zeros[Int](ROWCOL, ROWCOL)):Unit = {
-    val player:Player = Player.ai3
+  def play(implicit data: DenseMatrix[Int] = DenseMatrix.zeros[Int](ROWCOL, ROWCOL)) = {
+    val player = Player.ai3
+    player.states.clear
     player loadPolicy "c://work/tmp/player1-policy"
-    val otherPlayer:Player = Player.human
-    player.states= player.states.empty
+    val otherPlayer = Player.human
+    otherPlayer.states.clear
     go(player, otherPlayer)
   }
 }
