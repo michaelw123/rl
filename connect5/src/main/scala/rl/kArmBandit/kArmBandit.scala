c8753e81195710963f32ea7b803e73aae6e8d96a
michaelw123 <xiaoqunw@hotmail.com>
2017-11-04 13:22:42 -0400

    init chechin

--- connect5/src/main/scala/rl/kArmBandit/kArmBandit.scala
+++ connect5/src/main/scala/rl/kArmBandit/kArmBandit.scala
@@ -4,6 +4,8 @@ import breeze.linalg._
 import breeze.plot._
 import breeze.linalg.DenseVector
 import breeze.stats.distributions.Binomial
+import java.awt.{Color, Paint}
+import breeze.numerics._
 
 /*
   * Created by wangmich on 10/30/2017.
@@ -32,50 +34,55 @@ class Bandit (kArm: Int = 10, epsilon:Double = 0.0, stepSize:Double = 0.0) {
 
 object kArmBandit extends App{
 
-  //test
+  test
   epsilonGreedy(10, 10)
 
   private def test = {
-    val f = Figure()
-    val p = f.subplot(0)
-    val x = linspace(0.0,9.0, 10)
-    val y = x +:+ 2.0
-    p += plot(x, x :^ 2.0)
-    p += plot(x, x :^ 3.0, '.')
-    p += plot(x,y, '.')
 
-    p.xlabel = "x axis"
-    p.ylabel = "y axis"
-    f.saveas("lines.png")
+
+
+
+    //scatter(x, y, s)
+
+
   }
   def epsilonGreedy(nBandits:Int, time:Int) = {
     val epsilon = Seq(0, 0.1, 0.01)
     val bandits= (new Array[Bandit](nBandits)).map(_ => new Bandit(10, 0, 0))
     val bb = bandits.map(_ => new Bandit(10, 0, 0))
-    val (bestActionCount, agerageRewards) = banditSimulation(nBandits, time, bandits)
+    val (bestActionCount, averageRewards) = banditSimulation(nBandits, time, bandits)
     val f = Figure()
     val p = f.subplot(0)
-    val x = linspace(0.0,9.0, 10)
-    val y = x +:+ 2.0
-    val rows = bestActionCount(*, ::)
-    for (row <- rows) {
-      p += plot(x,row, '.')
-      //val x = 0
-    }
-    p += plot(x,y, '.')
-    p.xlabel = "x axis"
-    p.ylabel = "y axis"
+    val x = averageRewards(::, 0)
+    val y = averageRewards(::, 1)
+
+
+    p += scatter(averageRewards(::, 0), averageRewards(::, 1), {_ => 0.1},{a => Color.BLUE})
+    p.xlabel = "X-value"
+    p.ylabel = "Y-value"
+    p.title = "Input data"
+    //    p += plot(x,y, '.')
+    //    p.xlabel = "x axis"
+    //    p.ylabel = "y axis"
+  }
+  val y2Color: DenseVector[Double] => (Int => Paint) = y => {
+    case i => i2color(y(i).toInt)
+  }
+  val i2color: Int => Paint = _ match {
+    case 1 => Color.BLUE //accepted
+    case 0 => Color.RED //rejected
+    case _ => Color.BLACK //other
   }
   def banditSimulation(n:Int, time:Int, bandits:Array[Bandit]) = {
     val bestActionCounts = DenseMatrix.zeros[Int] (bandits.length, time)
     val averageRewards = DenseMatrix.zeros[Double] (bandits.length, time)
     for (b <- 0 to bandits.length-1; t <-0 to time-1) {
-        val action = bandits(b).getAction
-        val reward = bandits(b).takeAction(action)
-        averageRewards(b, t) += reward
-        if (action == bandits(b).bestAction) {
-          bestActionCounts(b,t) += 1
-        }
+      val action = bandits(b).getAction
+      val reward = bandits(b).takeAction(action)
+      averageRewards(b, t) += reward
+      if (action == bandits(b).bestAction) {
+        bestActionCounts(b,t) += 1
+      }
     }
     (bestActionCounts, averageRewards)
   }
