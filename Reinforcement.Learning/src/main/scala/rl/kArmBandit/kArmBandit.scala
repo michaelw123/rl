f9b874b0cba3459d41de0c01ecdb0e149b4f10d5
Michael Wang <xiaoqunw@hotmail.com>
2017-11-08 14:16:09 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/kArmBandit/kArmBandit.scala
+++ Reinforcement.Learning/src/main/scala/rl/kArmBandit/kArmBandit.scala
@@ -2,10 +2,9 @@ package rl.kArmBandit
 
 import breeze.linalg._
 import breeze.plot._
-import breeze.linalg.DenseVector
 import breeze.stats.distributions.Binomial
 import java.awt.{Color, Paint}
-import breeze.numerics._
+
 
 /*
   * Created by wangmich on 10/30/2017.
@@ -37,7 +36,7 @@ class Bandit (kArm: Int = 10, epsilon:Double = 0.0, stepSize:Double = 0.0) {
 object kArmBandit extends App{
 
   test
-  epsilonGreedy(10, 100)
+  epsilonGreedy(100, 100)
 
   private def test = {
 
@@ -51,15 +50,21 @@ object kArmBandit extends App{
   def epsilonGreedy(nBandits:Int, time:Int) = {
     val epsilon = Seq(0, 0.1, 0.01)
     val bandits= (new Array[Bandit](nBandits)).map(_ => new Bandit(10, 0, 0.1))
-    val (bestActionCount, averageRewards) = banditSimulation(nBandits, time, bandits)
+    val (bestActionCount, average) = banditSimulation(nBandits, time+1, bandits)
+   // val aaa=average1(0)
     val f = Figure()
     val p = f.subplot(0)
     //val x = averageRewards(::, 0)
     //val y = averageRewards(::, 1)
     //p += scatter(x, y, { _ => 0.1 }, { a => Color.BLUE })
 
-    for (i <- 0 to time -1) {
-      p += scatter(DenseVector.fill[Double](nBandits, i), averageRewards(::, i), { _ => 0.1 }, { a => Color.BLUE })
+//    for (i <- 0 to time -1) {
+//      p += scatter(DenseVector.fill[Double](nBandits, i), average1(i), { _ => 0.01 }, { a => Color.BLUE })
+//    }
+    var step = 0
+    for(col <- average(::, *)) {
+      p += scatter(DenseVector.fill[Double](nBandits, step), col, { _ => 0.1 }, { a => Color.BLUE })
+      step += 1
     }
     p.xlabel = "X-value"
      p.ylabel = "Y-value"
@@ -79,14 +84,27 @@ object kArmBandit extends App{
   def banditSimulation(n:Int, time:Int, bandits:Array[Bandit]) = {
     val bestActionCounts = DenseMatrix.zeros[Int] (bandits.length, time)
     val averageRewards = DenseMatrix.zeros[Double] (bandits.length, time)
-    for (b <- 0 to bandits.length-1; t <-0 to time-1) {
-      val action = bandits(b).getAction
-      val reward = bandits(b).takeAction(action)
-      averageRewards(b, t) += reward
-      if (action == bandits(b).bestAction) {
-        bestActionCounts(b,t) += 1
+    for (bandit <- bandits) {
+      for (i <- 0 to n-1) {
+        for (t <- 1 to time - 1) {
+          val action = bandit.getAction
+          val reward = bandit.takeAction(action)
+          averageRewards(i, t) += reward
+          if (action == bandit.bestAction) {
+            bestActionCounts(i, t) += 1
+          }
+        }
       }
     }
-    (bestActionCounts, averageRewards)
+//    val a = averageRewards(::, *) //broadcastedRows
+//    val b = averageRewards(*, ::) // broadcastedColumns
+//    val theSum = sum(averageRewards(*, ::))
+//    val theSum2 =  sum(averageRewards(::, *)).t
+//    val theAverage = theSum.map(_/time)
+////    val aa=DenseVector.zeros[Double]
+////
+////    val aaaa = aa.sum
+
+    (bestActionCounts, averageRewards.map(_/bandits.length) )
   }
 }
