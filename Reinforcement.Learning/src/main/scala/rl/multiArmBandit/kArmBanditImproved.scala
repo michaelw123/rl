874e864bff155f128b0f53346d4bd94d15040806
Michael Wang <xiaoqunw@hotmail.com>
2017-11-22 16:14:35 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/multiArmBandit/kArmBanditImproved.scala
+++ Reinforcement.Learning/src/main/scala/rl/multiArmBandit/kArmBanditImproved.scala
@@ -37,59 +37,51 @@ import scala.annotation.tailrec
 
 object multiArmBandit extends App {
   trait Arm
-  case class averageGreedyArm() extends Arm
+  case class averageGreedyArm(epsilon:Double) extends Arm
   case class incrementalArm(stepSize: Double) extends Arm
   case class ucbArm(ucb: Int) extends Arm
   case class gradientArm(stepSize: Double = 0) extends Arm
   case class initializationArm(initial: Double) extends Arm
 
-  class Bandit[T](arm: T, k: Int)(implicit action: Action[T]) {
-    val arms = List.fill[T](k)(arm)
+  trait Bandit[T]{//(implicit action: Action[T]) {
+    val k:Int = 10
+    var time=0
     val trueQ = DenseVector.fill[Double](k)(math.random)
     val qEstimation = DenseVector.fill[Double](k)(0)
     val actionCount = Array.fill[Int](k)(0)
     var actionProb = DenseVector.zeros[Double](k)
 
-    def getArm = action.getArm
-
-    def play(atm:T) = action.play(arm:T)
   }
+   class averageGreedyBandit(epsilon:Double) extends Bandit[averageGreedyArm] with Action{
+    val arms = List.fill[averageGreedyArm](k)(averageGreedyArm(epsilon))
 
-
-  trait Action[T] {
-    def getArm: T
-    def play[T](arm:T): Double
-  }
-  implicit object averageGreedyArmAction extends Action[averageGreedyArm] {
-    def getArm:averageGreedyArm= {
-      println("get averageGreedyArm")
-      averageGreedyArm()
+    def getArm:Int = {
+      System.out.println("getArm averageGreedyBandit")
+      if (Binomial(1, epsilon).draw == 1) scala.util.Random.nextInt(k) else  argmax(qEstimation)
     }
-    def play[averageGreedyArm](arm:averageGreedyArm) = {
-      println("play averageGreedyArm")
-      0.0
-    }
-  }
-  implicit object incrementalArmAction extends Action[incrementalArm] {
-    def getArm:incrementalArm = {
-      println("get incrementalArm")
-      incrementalArm(0.1)
-    }
-    def play[incrementalArm](arm:incrementalArm) = {
-      println("play incrementalArm")
-      0.0
+    def play(arm:Int):Double ={
+      System.out.println("play averageGreedyBandit")
+      val reward = trueQ.valueAt(arm) + math.random
+      time += 1
+      actionCount(arm) = actionCount(arm)+1
+      qEstimation(arm) += 1.0 / actionCount(arm) * (reward - qEstimation(arm))
+      reward
     }
   }
 
+  trait Action {
+    def getArm: Int
+    def play(arm:Int): Double
+  }
 
-  val bandit = new Bandit[averageGreedyArm](averageGreedyArm(), 10)
-  val arm:averageGreedyArm = bandit.getArm
+  val bandit =  new averageGreedyBandit(0.1)
+  val arm:Int = bandit.getArm
   bandit.play(arm)
 
-  val bandit2 = new Bandit[incrementalArm](incrementalArm(0.1), 10)
-  val arm2 = bandit2.getArm
-  bandit2.play(arm2)
-  val bandits = List.fill[Bandit[averageGreedyArm]](100)(new Bandit[averageGreedyArm](averageGreedyArm(), 10))
-  //bandits.foreach(_.getArm)
+//  val bandit2 = new Bandit[incrementalArm](incrementalArm(0.1), 10)
+//  val arm2 = bandit2.getArm
+//  bandit2.play(arm2)
+//  val bandits = List.fill[Bandit[averageGreedyArm]](100)(new Bandit[averageGreedyArm](averageGreedyArm(0.1), 10))
+//  //bandits.foreach(_.getArm)
 
 }
