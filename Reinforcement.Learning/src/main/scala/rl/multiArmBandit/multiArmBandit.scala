cfa02555680e1676f147a943bee9cdd675d8e317
Michael Wang <xiaoqunw@hotmail.com>
2017-11-23 14:15:21 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/multiArmBandit/multiArmBandit.scala
+++ Reinforcement.Learning/src/main/scala/rl/multiArmBandit/multiArmBandit.scala
@@ -54,10 +54,55 @@ object multiArmBandit extends App {
         reward
       }
     }
+    implicit object incrementalAlgorithm extends Algorithm[incrementalArm] {
+      def getArm(bandit: Bandit[incrementalArm]): Int = {
+        if (Binomial(1, bandit.arm.epsilon).draw == 1) scala.util.Random.nextInt(bandit.k) else argmax(bandit.qEstimation)
+      }
+      def play(bandit: Bandit[incrementalArm], arm: Int): Double = {
+        val reward = bandit.trueQ.valueAt(arm) + math.random
+        bandit.time += 1
+        bandit.actionCount(arm) = bandit.actionCount(arm) + 1
+        bandit.qEstimation(arm) += 1.0 / bandit.actionCount(arm) * (reward - bandit.qEstimation(arm))
+        bandit.qEstimation(arm) += bandit.arm.stepSize * (reward - bandit.qEstimation(arm))
+        reward
+      }
+    }
+  }
+  implicit object ucbAlgorithm extends Algorithm[ucbArm] {
+    def getArm(bandit: Bandit[ucbArm]): Int = {
+      argmax(
+        for ((est, count) <- bandit.qEstimation.toArray zip bandit.actionCount) yield  est + bandit.arm.ucb * sqrt(log(bandit.time+1)/(count+1))
+      )
+    }
+    def play(bandit: Bandit[ucbArm], arm: Int): Double = {
+      val reward = bandit.trueQ.valueAt(arm) + math.random
+      bandit.time += 1
+      bandit.actionCount(arm) = bandit.actionCount(arm) + 1
+      bandit.qEstimation(arm) += 1.0 / bandit.actionCount(arm) * (reward - bandit.qEstimation(arm))
+      reward
+    }
+  }
+  implicit object gradientAlgorithm extends Algorithm[gradientArm] {
+    def getArm(bandit: Bandit[gradientArm]): Int = {
+      val expEstimation = bandit.qEstimation.map(exp(_))
+      val theSum:Double = sum(expEstimation)
+      bandit.actionProb = expEstimation.map(a => a/theSum)
+      val index:Int = ExtendedRand.weightedChooseIndex( bandit.qEstimation.toArray,  bandit.actionProb.toArray).draw
+      index
+    }
+    def play(bandit: Bandit[gradientArm], arm: Int): Double = {
+      val reward = bandit.trueQ.valueAt(arm) + math.random
+      bandit.time += 1
+      bandit.actionCount(arm) = bandit.actionCount(arm)+1
+      val oneHot = DenseVector.zeros[Double](bandit.k)
+      oneHot(arm) = 1
+      bandit.qEstimation += (oneHot - bandit.actionProb) * bandit.arm.stepSize * reward
+      reward
+    }
   }
   trait Arm
   case class averageGreedyArm(epsilon:Double) extends Arm
-  case class incrementalArm(stepSize: Double) extends Arm
+  case class incrementalArm(epsilon:Double, stepSize: Double) extends Arm
   case class ucbArm(ucb: Int) extends Arm
   case class gradientArm(stepSize: Double = 0) extends Arm
   case class initializationArm(initial: Double) extends Arm
@@ -83,29 +128,139 @@ object multiArmBandit extends App {
     }
     (bestActionCounts, averageRewards.map(_/bandits.length) )
   }
-  val time = 1000
-  val f = Figure()
 
-  for (epsilon <- Seq(0.1, 0.2, 0.3)) {
-    val bandits = Array.fill(1000)(new Bandit(averageGreedyArm(epsilon)))
+  def averageGreedySimulation = {
     val time = 1000
-    val (bestActions, average) = banditSimulation(1000, time, bandits)
+    val f = Figure()
+    for (epsilon <- Seq(0.1, 0.2, 0.3)) {
+      val bandits = Array.fill(1000)(new Bandit(averageGreedyArm(epsilon)))
+      val time = 1000
+      val (bestActions, average) = banditSimulation(1000, time, bandits)
 
-    val p0 = f.subplot(0)
-    //val p1 = f.subplot(1)
-//    p0 += plot(linspace(0, time, time), sum(bestActions(::, *)).inner, colorcode =color(epsilon))
-//    p0.xlabel = "Steps"
-//    p0.ylabel = "Best Action"
-//    p0.title = "epsilon =" +epsilon
-    p0 += plot(linspace(0, time, time), mean(average(::, *)).inner, colorcode=color(epsilon))
-    p0.xlabel = "Steps"
-    p0.ylabel = "Average"
-    p0.title = "epsilon =" +epsilon
+      val p0 = f.subplot(0)
+      p0 += plot(linspace(0, time, time), sum(bestActions(::, *)).inner, colorcode = color(epsilon))
+      p0.xlabel = "Steps"
+      p0.ylabel = "Best Action"
+      p0.title = "epsilon =" + epsilon
+      //    p0 += plot(linspace(0, time, time), mean(average(::, *)).inner, colorcode=color(epsilon))
+      //    p0.xlabel = "Steps"
+      //    p0.ylabel = "Average"
+      //    p0.title = "epsilon =" +epsilon
+    }
+  }
+    def incrementalSimulation = {
+      val time = 1000
+      val f = Figure()
+      val epsilon=0.1
+      for (stepSize <- Seq(0.1, 0.2, 0.3)) {
+        val bandits = Array.fill(1000)(new Bandit(incrementalArm(epsilon, stepSize)))
+        val time = 1000
+        val (bestActions, average) = banditSimulation(1000, time, bandits)
+
+        val p0 = f.subplot(0)
+        p0 += plot(linspace(0, time, time), sum(bestActions(::, *)).inner, colorcode = color(epsilon))
+        p0.xlabel = "Steps"
+        p0.ylabel = "Best Action"
+        p0.title = "epsilon =" + epsilon
+        //    p0 += plot(linspace(0, time, time), mean(average(::, *)).inner, colorcode=color(epsilon))
+        //    p0.xlabel = "Steps"
+        //    p0.ylabel = "Average"
+        //    p0.title = "epsilon =" +epsilon
+      }
+  }
+  def ucbSimulation = {
+    val time = 1000
+    val f = Figure()
+    val epsilon=0.1
+    for (ucb <- Seq(1, 2, 3)) {
+      val bandits = Array.fill(1000)(new Bandit(ucbArm(ucb)))
+      val time = 1000
+      val (bestActions, average) = banditSimulation(1000, time, bandits)
+      val p0 = f.subplot(0)
+      p0 += plot(linspace(0, time, time), sum(bestActions(::, *)).inner, colorcode = color(ucb))
+      p0.xlabel = "Steps"
+      p0.ylabel = "Best Action"
+      p0.title = "UCB =" + ucb
+      //    p0 += plot(linspace(0, time, time), mean(average(::, *)).inner, colorcode=color(epsilon))
+      //    p0.xlabel = "Steps"
+      //    p0.ylabel = "Average"
+      //    p0.title = "epsilon =" +epsilon
+    }
+  }
+  def gradientSimulation = {
+    val time = 1000
+    val f = Figure()
+    val epsilon=0.1
+    for (gradient <- Seq(.1, .2, .3)) {
+      val bandits = Array.fill(1000)(new Bandit(gradientArm(gradient)))
+      val time = 1000
+      val (bestActions, average) = banditSimulation(1000, time, bandits)
+      val p0 = f.subplot(0)
+      p0 += plot(linspace(0, time, time), sum(bestActions(::, *)).inner, colorcode = color(gradient))
+      p0.xlabel = "Steps"
+      p0.ylabel = "Best Action"
+      p0.title = "Gragient =" + gradient
+    }
   }
   private def  color(epslon:Double):String = epslon match {
-    case 0.1 => "RED"
-    case 0.2 => "BLUE"
-    case 0.3 => "YELLOW"
+    case 0.1 | 1 => "RED"
+    case 0.2 | 2=> "BLUE"
+    case 0.3 | 3=> "YELLOW"
     case _ => "BLACK"
+  }
+
+  //averageGreedySimulation
+  //incrementalSimulation
+  //ucbSimulation
+  gradientSimulation
+
+
+  object ExtendedRand extends RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister())) {
+    def weightedChoose[T](c: Iterable[T], w:Iterable[Double]):Rand[T] = new Rand[T] {
+      var index = 0
+      val sample1 = Rand.uniform.draw
+      def draw() = {
+        val weightsArray = w.toArray
+        val d = go(0.0, weightsArray)
+        val elems = c.iterator
+        var i = 1
+        var e = elems.next()
+        while(i < d) {
+          e = elems.next()
+          i += 1
+        }
+        e
+      }
+      @tailrec
+      def go(s:Double, a:Array[Double]):Int = {
+        index += 1
+        if (s+a.head > sample1) index
+        else go(s+a.head, a.tail)
+      }
+    }
+    def weightedChooseIndex[T](c: Iterable[T], w:Iterable[Double]):Rand[Int] = new Rand[Int] {
+      var index = 0
+      val sample1 = Rand.uniform.draw
+      def draw() = {
+        val weightsArray = w.toArray
+        val d = go(0.0, weightsArray)
+        val elems = c.iterator
+        var i:Int = 1
+        var e = elems.next()
+        while (i < d) {
+          e = elems.next()
+          i += 1
+        }
+        i
+      }
+      @tailrec
+      def go(s: Double, a: Array[Double]): Int = {
+        if (s + a.head > sample1) index
+        else {
+          index += 1
+          go(s + a.head, a.tail)
+        }
+      }
+    }
   }
 }
