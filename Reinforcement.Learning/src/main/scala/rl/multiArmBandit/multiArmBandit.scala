ad40fbe4cb6b5b97a2058ddf71d657920762f15a
Michael Wang <xiaoqunw@hotmail.com>
2017-11-24 13:03:41 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/multiArmBandit/multiArmBandit.scala
+++ Reinforcement.Learning/src/main/scala/rl/multiArmBandit/multiArmBandit.scala
@@ -20,9 +20,10 @@
  */
 /*
   assuming 10 arm bandit - this configuration can be changed easily
-  arm: contains features of the bandit -
-  Algorithm: foure althorithm are implemented by typeclass Algorithm: epsilon greedy, incremental, gradient, and ucb
+  arm: contains features of the bandit
+  Algorithm: four althorithms are implemented by typeclass Algorithm: epsilon greedy, incremental, gradient, and ucb
   scalanlp's breeze is used for numerical computation, and breeze-viz for data visualization
+  ExtendedRand is an extension of breeze Rand, which adds "weighted choose by index" from an Iterable, or typically, DenseVector.
  */
 
 package rl.multiArmBandit
