73ee1fc1be044e7142f5716544f8e532f25dc1a1
michaelw123 <xiaoqunw@hotmail.com>
2017-11-26 14:25:58 -0500

    init chechin

---
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -0,0 +1,24 @@
+package rl.mdp
+
+import breeze.linalg.DenseVector
+
+/**
+  * Created by MichaelXiaoqun on 2017-11-26.
+  */
+object gridWorld {
+  val NORTH=0
+  val EAST=1
+  val SOUTH=2
+  val WEST=3
+  val WORLDSIZE=5
+  val A=(0,1)
+  val PRIMEA=(4,1)
+  val B=(0,3)
+  val PROMEB=(2,3)
+  val actions = DenseVector(NORTH, EAST, SOUTH, WEST)
+
+
+
+
+
+}
