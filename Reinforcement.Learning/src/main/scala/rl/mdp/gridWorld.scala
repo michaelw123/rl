c235dc7dabf01cc931c24de6bdc8118c286cfd42
michaelw123 <xiaoqunw@hotmail.com>
2017-11-26 19:53:11 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -1,21 +1,35 @@
 package rl.mdp
 
-import breeze.linalg.DenseVector
+import java.awt.Desktop.Action
+
+import breeze.linalg.{DenseMatrix, DenseVector}
 
 /**
   * Created by MichaelXiaoqun on 2017-11-26.
   */
 object gridWorld {
-  val NORTH=0
-  val EAST=1
-  val SOUTH=2
-  val WEST=3
-  val WORLDSIZE=5
-  val A=(0,1)
-  val PRIMEA=(4,1)
-  val B=(0,3)
-  val PROMEB=(2,3)
-  val actions = DenseVector(NORTH, EAST, SOUTH, WEST)
+  trait Action
+  sealed
+  case object North extends Action
+  case object East extends Action
+  case object South extends Action
+  case object West extends Action
+
+
+
+  class GridWorld {
+    val WORLDSIZE = 5
+    val A = (0, 1)
+    val PRIMEA = (4, 1)
+    val B = (0, 3)
+    val PROMEB = (2, 3)
+   val grid = DenseMatrix.fill[State](WORLDSIZE, WORLDSIZE)(State(North,0,0,0.1))
+  }
+  val actions = DenseVector(North, East, South, West)
+  case class State (action:Action, x:Int, y:Int, reward:Double)
+  val s=State(North, 0, 0, 0.2)
+
+
 
 
 
