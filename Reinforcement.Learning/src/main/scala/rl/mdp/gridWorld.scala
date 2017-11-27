c6eb92c46fa921700bd58bb882d3df7801d794dc
Michael Wang <xiaoqunw@hotmail.com>
2017-11-27 15:51:45 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -15,7 +15,7 @@ object gridWorld extends App {
   case class South(value:Int=2) extends Action
   case class West(value:Int=3) extends Action
 
-val DISCOUNT = 0.9
+  val DISCOUNT = 0.9
   val ACTIONPROB=0.25
     val WORLDSIZE = 5
     val WORLDSIZE1 = WORLDSIZE-1
@@ -26,7 +26,7 @@ val DISCOUNT = 0.9
     var grid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
       (i, j) => new Node(i, j)
     }
-    def policy[T](a: T, b: (Int, Int)): ((Int, Int), Double) = (a, (b._1, b._2)) match {
+    def policy[T](a: T, b: (Int, Int)): ((Int, Int), Double) = (a, b) match {
       case (_, A) => (PRIMEA, 10)
       case (_, B) => (PRIMEB, 5)
       case (North, (0, _)) => ((0, b._2), -1)
@@ -37,6 +37,7 @@ val DISCOUNT = 0.9
       case (South, (_, _)) => ((b._1 + 1, b._2), 0)
       case (West, (_, 0)) => ((b._1, 0), -1)
       case (West, (_, _)) => ((b._1, b._2 - 1), 0)
+      case (_, (_, _)) => ((b._1, b._2), 0)
     }
     case class Node(val x: Int, val y: Int) {
 
@@ -47,31 +48,23 @@ val DISCOUNT = 0.9
       val south = policy(South, (x, y))
       val west = policy(West, (x, y))
     }
-    for (i <- 0 until 1000) {
-      //bellman
-      valueIteration
+    for (i <- 0 until 5000) {
+      bellman
+      //valueIteration
     }
-    println(grid.map(a => (rounded(3, a.value))))
+    println(grid.map(a => rounded(3, a.value)))
 
-
-
-
-
-    def bellman = {
-    val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
-      (i, j) => new Node(i, j)
-    }
-    for (i <- 0 until WORLDSIZE; j <- 0 until WORLDSIZE) {
-      newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).north._2 + DISCOUNT * grid(newGrid(i,j).north._1._1, newGrid(i,j).north._1._2).value)
-      newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).east._2 + DISCOUNT * grid(newGrid(i,j).east._1._1, newGrid(i,j).east._1._2).value)
-      newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).south._2 + DISCOUNT * grid(newGrid(i,j).south._1._1, newGrid(i,j).south._1._2).value)
-      newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).west._2 + DISCOUNT * grid(newGrid(i,j).west._1._1, newGrid(i,j).west._1._2).value)
-    }
-    grid = newGrid
-  }
-  def rounded(x: Int, n:Double) = {
-    val w = math.pow(10, x)
-    (n * w).toLong.toDouble / w
+  def bellman = {
+      val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
+        (i, j) => new Node(i, j)
+      }
+      for (i <- 0 until WORLDSIZE; j <- 0 until WORLDSIZE) {
+       newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).north._2 + DISCOUNT * grid(newGrid(i,j).north._1._1, newGrid(i,j).north._1._2).value)
+        newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).east._2 + DISCOUNT * grid(newGrid(i,j).east._1._1, newGrid(i,j).east._1._2).value)
+        newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).south._2 + DISCOUNT * grid(newGrid(i,j).south._1._1, newGrid(i,j).south._1._2).value)
+       newGrid(i,j).value += ACTIONPROB * (newGrid(i,j).west._2 + DISCOUNT * grid(newGrid(i,j).west._1._1, newGrid(i,j).west._1._2).value)
+      }
+      grid = newGrid
   }
   def valueIteration = {
     val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
@@ -85,10 +78,9 @@ val DISCOUNT = 0.9
       newGrid(i,j).value = max(Seq(v1, v2, v3, v4))
     }
     grid = newGrid
-
-
-   // values.append(actionReward[i][j][action] + discount * world[newPosition[0], newPosition[1]])
-
   }
-
+  def rounded(x: Int, n:Double) = {
+    val w = math.pow(10, x)
+    (n * w).toLong.toDouble / w
+  }
 }
