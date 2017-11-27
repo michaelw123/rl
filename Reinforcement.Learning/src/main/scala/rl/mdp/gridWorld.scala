fdd10025ff13a9b176e4caa5f420e45a7e9bef73
michaelw123 <xiaoqunw@hotmail.com>
2017-11-26 21:37:48 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -14,25 +14,37 @@ object gridWorld {
   case object East extends Action
   case object South extends Action
   case object West extends Action
-
-
+  val WORLDSIZE = 4
 
   class GridWorld {
-    val WORLDSIZE = 5
     val A = (0, 1)
     val PRIMEA = (4, 1)
     val B = (0, 3)
     val PROMEB = (2, 3)
-   val grid = DenseMatrix.fill[Node](WORLDSIZE, WORLDSIZE)
+    val grid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE){
+      (i,j) => new Node(i,j)
+    }
   }
   class Node(x:Int, y:Int) {
     var value:Double=0
-    val north:(Action, Node, Double) = (North, this, 0)
-    val east:(Action, Node, Double) = (East, this, 0)
-    val south:(Action, Node,Double) = (South, this, 0)
-    val west:(Action,Node, Double) = (West, this, 0)
+    var north = (x, y) match {
+      case (0, _) => ((0, y), 0)
+      case (_, _) => ((x - 1, y), 0)
+    }
+    var east = (x, y) match {
+      case (_, WORLDSIZE) => ((x, WORLDSIZE), 0)
+      case (_, _) => ((x, y+1), 0)
+    }
+    val south= (x, y) match {
+      case (WORLDSIZE, _) => ((WORLDSIZE, y), 0)
+      case (_, _) => ((x+1, y), 0)
+    }
+    val west = (x, y) match {
+      case (_, 0) => ((x, 0), 0)
+      case (_, _) => ((x, y-1), 0)
+    }
   }
-  val actions = DenseVector(North, East, South, West)
+
 
 
 
