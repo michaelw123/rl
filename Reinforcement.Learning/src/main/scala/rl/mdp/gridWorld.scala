5e789a87832327a953e177995e230fdd411e5028
Michael Wang <xiaoqunw@hotmail.com>
2017-11-29 14:36:07 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -1,6 +1,32 @@
+/*
+ * Copyright (c) 2017 Michael Wang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+/*
+  Implementation Details
+  Assuming 10 arm bandit - this configuration can be changed easily
+  arm: contains features of the bandit
+  Algorithm: four althorithms are implemented by typeclass Algorithm: epsilon greedy, incremental, gradient, and ucb
+  scalanlp's breeze is used for numerical computation, and breeze-viz for data visualization
+  ExtendedRand is an extension of breeze Rand, which adds "weighted choose by index" from an Iterable, or typically, DenseVector.
+ */
 package rl.mdp
-
-import java.awt.Desktop.Action
 
 import breeze.linalg.{DenseMatrix, DenseVector, max}
 
