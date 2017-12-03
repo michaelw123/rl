571971e340b02195fa5997e148fb343afef58e4a
michaelw123 <xiaoqunw@hotmail.com>
2017-12-03 12:44:10 -0500

    init chechin

--- Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
+++ Reinforcement.Learning/src/main/scala/rl/mdp/gridWorld.scala
@@ -26,7 +26,7 @@ import breeze.linalg.{DenseMatrix, DenseVector, max}
 /**
   * Created by Michael Wang on 2017-11-26.
   */
-  object gridWorld extends App {
+object gridWorld extends App {
     trait Action
     sealed
     case class North(value:Int=0) extends Action
@@ -36,62 +36,62 @@ import breeze.linalg.{DenseMatrix, DenseVector, max}
 
     val DISCOUNT = 0.9
     val ACTIONPROB=0.25
-      val WORLDSIZE = 5
-      val WORLDSIZE1 = WORLDSIZE-1
-      val A = (0, 1)
-      val PRIMEA = (4, 1)
-      val B = (0, 3)
-      val PRIMEB = (2, 3)
-      var grid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
-        (i, j) => new Node(i, j)
-      }
-      def policy[T](a: T, b: (Int, Int)): ((Int, Int), Double) = (a, b) match {
-        case (_, A) => (PRIMEA, 10)
-        case (_, B) => (PRIMEB, 5)
-        case (North, (0, _)) => (b, -1)
-        case (North, b) => ((b._1 - 1, b._2), 0)
-        case (East, (_, WORLDSIZE1)) => (b, -1)
-        case (East, b) => ((b._1, b._2 + 1), 0)
-        case (South, (WORLDSIZE1, _)) => (b, -1)
-        case (South, b) => ((b._1 + 1, b._2), 0)
-        case (West, (_, 0)) => (b, -1)
-        case (West, b) => ((b._1, b._2 - 1), 0)
-        case (_, _) => (b, 0)
-      }
-
-      case class Node(val x: Int, val y: Int) {
-        var value: Double = 0
-        val actions = Seq( policy(North, (x, y)),
-                  policy(East, (x, y)),
-                  policy(South, (x, y)),
-                  policy(West, (x, y)))
-      }
-      for (i <- 0 until 5000) {
-        bellman
-       // valueIteration
-      }
-      println(grid.map(a => rounded(3, a.value)))
-
-    def bellman = {
-        val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
-          (i, j) =>  Node(i, j)
-        }
-        newGrid.toArray.foreach { node =>
-          node.value = node.actions.foldLeft(node.value)((a,b) =>  a + ACTIONPROB * (b._2 + DISCOUNT * grid(b._1).value))
-        }
-        grid = newGrid
+    val WORLDSIZE = 5
+    val WORLDSIZE1 = WORLDSIZE-1
+    val A = (0, 1)
+    val PRIMEA = (4, 1)
+    val B = (0, 3)
+    val PRIMEB = (2, 3)
+    var grid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
+      (i, j) => new Node(i, j)
     }
-    def valueIteration = {
+    def policy[T](a: T, b: (Int, Int)): ((Int, Int), Double) = (a, b) match {
+      case (_, A) => (PRIMEA, 10)
+      case (_, B) => (PRIMEB, 5)
+      case (North, (0, _)) => (b, -1)
+      case (North, b) => ((b._1 - 1, b._2), 0)
+      case (East, (_, WORLDSIZE1)) => (b, -1)
+      case (East, b) => ((b._1, b._2 + 1), 0)
+      case (South, (WORLDSIZE1, _)) => (b, -1)
+      case (South, b) => ((b._1 + 1, b._2), 0)
+      case (West, (_, 0)) => (b, -1)
+      case (West, b) => ((b._1, b._2 - 1), 0)
+      case (_, _) => (b, 0)
+    }
+
+    case class Node(val x: Int, val y: Int) {
+      var value: Double = 0
+      val actions = Seq( policy(North, (x, y)),
+                policy(East, (x, y)),
+                policy(South, (x, y)),
+                policy(West, (x, y)))
+    }
+    for (i <- 0 until 5000) {
+      bellman
+     // valueIteration
+    }
+    println(grid.map(a => rounded(3, a.value)))
+
+  def bellman = {
       val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
-        (i, j) => new Node(i, j)
+        (i, j) =>  Node(i, j)
       }
       newGrid.toArray.foreach { node =>
-        node.value = max(node.actions.map(a => a._2 + DISCOUNT * grid(a._1).value))
+        node.value = node.actions.foldLeft(node.value)((a,b) =>  a + ACTIONPROB * (b._2 + DISCOUNT * grid(b._1).value))
       }
       grid = newGrid
-    }
-    def rounded(x: Int, n:Double) = {
-      val w = math.pow(10, x)
-      (n * w).toLong.toDouble / w
-    }
   }
+  def valueIteration = {
+    val newGrid = DenseMatrix.tabulate[Node](WORLDSIZE, WORLDSIZE) {
+      (i, j) => new Node(i, j)
+    }
+    newGrid.toArray.foreach { node =>
+      node.value = max(node.actions.map(a => a._2 + DISCOUNT * grid(a._1).value))
+    }
+    grid = newGrid
+  }
+  def rounded(x: Int, n:Double) = {
+    val w = math.pow(10, x)
+    (n * w).toLong.toDouble / w
+  }
+}
