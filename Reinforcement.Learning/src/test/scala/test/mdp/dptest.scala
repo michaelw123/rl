1497b3209088cbd5fb50c3ef0ad483c1fbfd685b
michaelw123 <xiaoqunw@hotmail.com>
2017-12-17 17:08:46 -0500

    mdp core

--- Reinforcement.Learning/src/test/scala/test/mdp/dptest.scala
+++ Reinforcement.Learning/src/test/scala/test/mdp/dptest.scala
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2017 Michael Wang
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 package test.mdp
 
 import breeze.linalg.DenseVector
@@ -5,12 +25,12 @@ import rl.core.mdp.Environment
 import rl.core.mdp.FlatWorld.flatWorldAction.{East, North, South, West}
 import rl.core.mdp.FlatWorld.{flatWorldAction, flatWorldAgent, flatWorldPolicy, flatWorldState}
 import rl.utils.rounded
-import test.mdp.mdpTest.result
 
 import scala.reflect.ClassTag
 
 /**
   * Created by Michael Wang on 2017-12-17.
+  * To test Dynamic Programming for MDP
   */
 object dptest extends App{
   val SIZE = 16
